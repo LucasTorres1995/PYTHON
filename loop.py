83ee56b169ef20deba1b0edb0acef7d64467ccb2
LucasTorres1995 <lucasgbtorres@gmail.com>
2022-07-14 00:15:40 -0300

    14072022

--- loop.py
+++ loop.py
@@ -23,3 +23,56 @@
 # for celular in celular:
 #     for versao in versoes:
 #         print(f'{celular} {versao}')
+
+# -------------------------- ITERÁVEL
+# PASSA TODAS AS INFORMAÇÕES CONTIDAS DENTRO DAS STRINGS
+
+# for item in range(10):
+#     print(item)
+
+# for letra in 'programação':
+#     print(letra)
+
+# for iteem in [2,3,4,5]:
+#     print(iteem)
+
+
+# ------------------------ LOOP WILE (LAÇO WHILE)
+
+# tentativas = 0
+# while tentativas < 3:
+#     print('Tente novamente')
+#     tentativas = tentativas + 1
+#   tentativas += 1
+
+# SENHA
+# senha = ''
+# while senha != '123456':
+#     senha = input ('Digite sua senha: ')
+# print ('Bem-vindo!')
+
+# RECEBER NOME DO USUARIO
+# nome = ''
+# while nome == '':
+#     nome = input ('Digite seu nome: ')
+# print(f'Bem-vindo {nome}')
+
+
+# CONTAGEM E CONTAGEM REGRESSIVA
+
+# horario = 0
+# while horario <= 17:
+#     print(horario)
+#     horario += 1
+# print('hora de ver o por do sol')
+
+# # REGRESSIVA
+# contador = 100
+# while contador >= 0:
+#     print(contador)
+#     contador -= 1
+
+# # PASS SERVE PARA FAZER O PROGRAMA IGNORAR O ERRO, É COMO DIZER AO PYTHON " EU SEI QUE ESTA FALTANDO INFORMAÇÃO, MAS DEPOIS EU COLOCO"
+# numero = 0
+# while numero < 5:
+#     pass # falta informação aqui, mas o programa irá ignorar
